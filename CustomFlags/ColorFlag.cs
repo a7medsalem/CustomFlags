1b44546ef2144940ee8cc3ac518659b1081f1672
a7medsalem <its2hardway@gmail.com>
2021-10-09 17:59:48 +0200

    Upload projects files

---
+++ CustomFlags/ColorFlag.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomFlags
+{
+    internal class ColorFlag : Flag
+    {
+        public static ColorFlag NON     = new ColorFlag(false);
+        public static ColorFlag RED     = new ColorFlag(1);
+        public static ColorFlag YELLOW  = new ColorFlag(2);
+        public static ColorFlag BLUE    = new ColorFlag(3);
+        public static ColorFlag BLACK   = new ColorFlag(4);
+        public static ColorFlag WHITE   = new ColorFlag(5);
+        public static ColorFlag ALL     = new ColorFlag(true);
+        
+        private const int LENGTH = 128;
+        private ColorFlag(bool allTrue) : base(LENGTH, allTrue) { }
+        private ColorFlag(params int[] indices) : base(LENGTH, indices) { }
+    }
+}
---
+++ CustomFlags/Flag.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFlags
+{
+    public class Flag
+    {
+        private byte[] _flags;
+        private Flag(byte[] flags)
+        {
+            this._flags = flags;
+        }
+        public Flag(int length, bool allTrue = false)
+        {
+            if (length < 0)
+                throw new ArgumentOutOfRangeException("Lenght can't be zero.");
+
+            this._flags = new byte[length];
+            for (int i = 0; i < length; i++)
+            {
+                this._flags[i] = (byte)(allTrue ? 1 : 0);
+            }
+        }
+        public Flag(int length, params int[] indices)
+        {
+            if (length < 0)
+                throw new ArgumentOutOfRangeException("Lenght can't be zero.");
+
+            if (indices.Length > 0)
+            {
+                int minIndex = indices.Min();
+                if (minIndex < 0)
+                    throw new ArgumentOutOfRangeException("Index can't be negative number.");
+
+                int maxIndex = indices.Max();
+                if (maxIndex >= length)
+                    throw new ArgumentOutOfRangeException("Max index must be less than given length.");
+            }
+
+            this._flags = new byte[length];
+            for (int i = 0; i < indices.Length; i++)
+            {
+                this._flags[indices[i]] = 1;
+            }
+        }
+        protected bool ExceedMaxInt()
+        {
+            return this._flags.Length > 64 && this._flags.TakeWhile((b, i) => i > 64 && b > 0).Any();
+        }
+        protected int ToInteger()
+        {
+            int index = 0;
+            return this._flags.Aggregate(0, (sum, b) => (int)(sum + b * Math.Pow(2, index++)));
+        }
+
+        private static Flag Bitwise(Flag first, Flag other, Func<byte, byte, byte> operation)
+        {
+            if (first == null || other == null)
+                throw new ArgumentNullException("Can't bitwise null objects");
+
+            if (first._flags.Length != other._flags.Length)
+                throw new ArgumentException("Both flags must be same length");
+
+            byte[] resultOnes = new byte[first._flags.Length];
+            for (int i = 0; i < first._flags.Length; i++)
+            {
+                resultOnes[i] = operation(first._flags[i], other._flags[i]);
+            }
+
+            return new Flag(resultOnes);
+        }
+        public static Flag operator |(Flag self, Flag flag)
+        {
+            return Bitwise(self, flag, (f, o) => (byte)(f | o));
+        }
+        public static Flag operator &(Flag self, Flag flag)
+        {
+            return Bitwise(self, flag, (f, o) => (byte)(f & o));
+        }
+        public static bool operator true(Flag flag)
+        {
+            if (flag == null) return false;
+            //
+            return flag._flags.Any(f => f > 0);
+        }
+        public static bool operator false(Flag flag)
+        {
+            if (flag == null) return false;
+            //
+            return flag._flags.All(f => f == 0);
+        }
+        public static bool operator >(Flag flag, int integer)
+        {
+            if (flag == null)
+                throw new ArgumentNullException("Can't compare null flag.");
+
+            if (flag.ExceedMaxInt())
+                return true;
+
+            return flag.ToInteger() > integer;
+        }
+        public static bool operator >=(Flag flag, int integer)
+        {
+            if (flag == null)
+                throw new ArgumentNullException("Can't compare null flag.");
+
+            if (flag.ExceedMaxInt())
+                return true;
+
+            return flag.ToInteger() >= integer;
+        }
+        public static bool operator <(Flag flag, int integer)
+        {
+            if (flag == null)
+                throw new ArgumentNullException("Can't compare null flag.");
+
+            if (flag.ExceedMaxInt())
+                return false;
+
+            return flag.ToInteger() < integer;
+        }
+        public static bool operator <=(Flag flag, int integer)
+        {
+            if (flag == null)
+                throw new ArgumentNullException("Can't compare null flag.");
+
+            if (flag.ExceedMaxInt())
+                return false;
+
+            return flag.ToInteger() <= integer;
+        }
+        public static explicit operator int (Flag flag)
+        {
+            if (flag.ExceedMaxInt())
+                return int.MaxValue;
+            else
+                return flag.ToInteger();
+        }
+    }
+}
